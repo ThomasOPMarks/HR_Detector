3eed7c052ef8a2dc4d426e232ce87054c717305f
Sydney Brookstein <sydneybrookstein@Sydneys-MacBook-Pro.local>
2019-03-22 11:58:42 -0600

    added comments

--- app/src/main/java/com/example/hr_detector/MainActivity.java
+++ app/src/main/java/com/example/hr_detector/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.hr_detector;
 
-import android.content.Context;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.activity.WearableActivity;
+import android.telephony.mbms.MbmsErrors;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
@@ -13,66 +12,52 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends WearableActivity implements SensorEventListener {
 
-    private TextView mTextView;
+    private TextView sTextView;
     private TextView cTextView;
     private int target_heart_rate;
     private int numClicked;
     private int current_heart_rate;
     private SensorManager mSensorManager;
     private Sensor mHeartRateSensor;
-    //private SensorEventListener sensorEventListener;
-
-
-    @Override
-    public void onSensorChanged(SensorEvent event){
-        if(event.sensor.getType() == Sensor.TYPE_HEART_RATE)
-        {
-            current_heart_rate = (int) event.values[0]; //this is the certainty, not the actual HR value?
-        }
-
-        String TAG = "tag";
-        Log.i(TAG, "--------------------------");
-        Log.i(TAG, "message");
-        Log.i(TAG, "" + event.sensor.getType());
-        Log.i("live", "--------------");
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //do nothing for now
-
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize all values to 0
         target_heart_rate = 0;
         numClicked = 0;
         current_heart_rate = 0;
 
-        mTextView = (TextView) findViewById(R.id.selectHR);
+        //Set text views, one to display target HR and the other to display current HR
+        sTextView = (TextView) findViewById(R.id.selectHR);
         cTextView = (TextView) findViewById(R.id.current);
 
-        cTextView.setText("Current Heart Rate: " + current_heart_rate);
+        //Initialize text to display current HR and set to invisible
+        cTextView.setText("Current Heart Rate: ...");
         cTextView.setVisibility(View.INVISIBLE);
 
-
+        //Initialize HR sensor, and sensor manager
         mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
         mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+        //get permission to test HR, if this is the first time the app is being used on this device
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{android.Manifest.permission.BODY_SENSORS},
+                1);
 
-
+        //Initialize number picker with values from 40 to 180
         final NumberPicker np = (NumberPicker) findViewById(R.id.number_picker);
-
         np.setMinValue(40);
         np.setMaxValue(180);
 
+        //Listener for the number picker. Sets target HR to selected value
         np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
@@ -80,22 +65,23 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             }
         });
 
-
+        //Next/Back button
         final Button next = findViewById(R.id.nextButton);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
+                //If the user has clicked here, the target HR is set up, show current HR
                 if(numClicked == 0){
-                    np.setVisibility(v.INVISIBLE);
-                    mTextView.setText("Target Heart Rate: " + target_heart_rate);
+                    sTextView.setText("Target Heart Rate: " + target_heart_rate);
                     cTextView.setVisibility(v.VISIBLE);
+                    np.setVisibility(v.INVISIBLE);
                     next.setText("BACK");
                     numClicked++;
+                //If the user has clicked again, go back to first screen to display number picker
                 } else{
-                    cTextView.setText("Current Heart Rate: " + current_heart_rate);
                     cTextView.setVisibility(v.INVISIBLE);
+                    //cTextView.setText("Current Heart Rate: ...");
                     np.setVisibility(v.VISIBLE);
-                    mTextView.setText("Select Heart Rate");
+                    sTextView.setText("Select Heart Rate");
                     next.setText("NEXT");
                     numClicked--;
                 }
@@ -103,14 +89,60 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             }
         });
 
-
-
-
-
-
-
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    //Requests permission to read HR, because it is sensitive information
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.i("got event", "onRequestPermissionsResult: " + requestCode);
+
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                    Log.i("permsGranted", "we have permission registering the heartrate sensor");
+                    mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+                } else {
+                    //not granted
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    @Override
+    //Handles HR change
+    public void onSensorChanged(SensorEvent event){
+
+        if(event.sensor.getType() == Sensor.TYPE_HEART_RATE)
+        {
+            String msg = "" + (int)event.values[0];
+            current_heart_rate = (int) event.values[0];
+            sTextView.setText("Target Heart Rate: " + target_heart_rate);
+            cTextView.setText("Current Heart Rate: " + current_heart_rate);
+            sTextView.setVisibility(View.VISIBLE);
+            cTextView.setVisibility(View.VISIBLE);
+            if (current_heart_rate >= target_heart_rate) { //Target HR has been achieved
+                //Display message showing Target HR is reached, in the future send signal for Bluno to turn on
+                cTextView.setText("Signal bluetooth device that the mood has been achieved!!");
+                current_heart_rate = 0;
+                //Turn off HR monitor to save battery
+                mSensorManager.unregisterListener(this,mHeartRateSensor);
+            }
+        }
+
+    }
+
+    @Override
+    //Handles accuracy change. Currently does nothing.
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //log change in sensor accuracy
+        Log.i("onAccuracyChanged", "-----------------------------------------------------------------------------------------------onAccuracyChanged - accuracy: " + accuracy);
     }
 }
 
